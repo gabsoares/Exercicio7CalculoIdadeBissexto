1137ea522571d399f637bd0176f18899f4da1b49
gabsoares <soaresgabriel68@gmail.com>
2024-05-05 01:20:18 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio7CalculoIdadeBissexto/Pessoa.cs
@@ -0,0 +1,60 @@
+﻿namespace Exercicio7CalculoIdadeBissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        int idade;
+        string profissao;
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public void setProfissao(string profissao)
+        {
+            this.profissao = profissao;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public int getIdade()
+        {
+            return idade;
+        }
+
+        public string getProfissao()
+        {
+            return profissao;
+        }
+
+        public string getIdadeBissexto()
+        {
+            int idade, anoBissexto = 0;
+            int anoAtual = DateTime.Now.Year;
+            Console.Write("Digite o ano de seu nascimento: ");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNascimento;
+
+            for (int i = anoNascimento; i < anoAtual; i++)
+            {
+                if ((i % 400 == 0) || (i % 4 == 0 && i % 100 != 0))
+                {
+                    anoBissexto++;
+                }
+            }
+            return $"{idade}\nIdade em Bissexto: {anoBissexto}";
+        }
+        public override string? ToString()
+        {
+            return $"\nImpressão\n\nNome: {nome}\nIdade: {getIdadeBissexto()}\nProfissão: {profissao}";
+        }
+    }
+}
---
+++ Exercicio7CalculoIdadeBissexto/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exercicio7CalculoIdadeBissexto;
+
+Pessoa p1 = new();
+
+Console.Write("Nome: ");
+p1.setNome(Console.ReadLine());
+Console.Write("Profissao: ");
+p1.setProfissao(Console.ReadLine());
+
+
+Console.WriteLine(p1.ToString());
